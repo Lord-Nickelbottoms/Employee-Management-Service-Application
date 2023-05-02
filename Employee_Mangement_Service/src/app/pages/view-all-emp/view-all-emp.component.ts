2c273e87a81451c1549833a6bc31cfa7238d36c2
Lord-Nickelbottoms <nizaam.h@icloud.com>
2023-05-02 16:05:09 +0200

    added CRUD functions

--- Employee_Mangement_Service/src/app/pages/view-all-emp/view-all-emp.component.ts
+++ Employee_Mangement_Service/src/app/pages/view-all-emp/view-all-emp.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+// import { EmployeeServiceService } from './../../../services/employee-service.service';
+import { any } from '0g';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/employee';
+import { EmployeeServiceService } from 'src/app/services/employee-service.service';
 
 @Component({
   selector: 'app-view-all-emp',
   templateUrl: './view-all-emp.component.html',
   styleUrls: ['./view-all-emp.component.css']
 })
-export class ViewAllEmpComponent {
+export class ViewAllEmpComponent implements OnInit{
+  employees: any;
 
+  constructor(private employeeService: EmployeeServiceService, private router: Router){}
+
+  ngOnInit(): void {
+      this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeeList().subscribe(data=> {
+      this.employees = data;
+      console.log(this.employees)
+    });
+  }
+
+  employeeDetails(id: number){
+    this.router.navigate(['employee-details', id]);
+  }
+
+  updateEmployee(id: number){
+    this.router.navigate(['update-employee', id]);
+  }
+
+  // deleteEmployee(id: number){
+  //   this.employeeService.deleteEmployee(id).subscribe( data => {
+  //     console.log(data);
+  //     this.getEmployees();
+  //   })
+  // }
 }
